f7450fa39a6613b34e5bfc9fb6ddf12fc622e12b
Leora Juster <leora.juster@hyfn.com>
2018-03-11 13:37:25 -0700

    Iniital set-up for api routing and json constraint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  respond_to :json
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    return if request.format == :json
+    render :nothing => true, :status => 406
+  end
 end
